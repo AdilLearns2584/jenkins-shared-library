74f94a5a0c079deded618386d33f08f9e7f0dfa5
talk2ad25 <talk2ad25@gmail.com>
2026-02-14 13:02:27 +0530

    changes

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,18 @@
+#!/user/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+    def script
+    Docker(script){
+        this.script =  script
+    }
+
+    def buildDockerImage(String imageName){
+        script.echo "Building the Application"
+        script.withCredentials([usernamePassword(credentialsId: 'dockerhub-repo',passwordVariable:'PASS',usernameVariable:'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
